6114fee099407950e352bc8360227dff12093831
Sergey <sergeyrez.sr@gmail.com>
2020-04-08 11:07:10 +0300

    Ready

---
+++ ServiceStation/Classes/Autocar_BACKUP_1082.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceStation.Classes
+{
+    public abstract class Autocar
+    {
+        public AutoCompany ManufacturedCompany { get; private set; }
+        public string ModelCipher { get; private set; }
+
+        public EngineTypes EngineType { get; private set; }
+
+<<<<<<< HEAD
+        public Autocar(AutoCompany manufacturedCompany, string modelCipher, EngineTypes engineTypes)
+=======
+        public decimal EngineVolume { get; private set; }
+
+        public Autocar(string manufacturedCompany, string modelCipher, EngineTypes engineTypes = EngineTypes.Gas, decimal engineVolume = 2)
+>>>>>>> 1c25bc0404f337966759a96884c71694ffa414f8
+        {
+            ManufacturedCompany = manufacturedCompany;
+            ModelCipher = modelCipher;
+            EngineType = engineTypes;
+            EngineVolume = engineVolume;
+        }
+
+        public virtual string GetFullName()
+        {
+            return $"{ManufacturedCompany}, {ModelCipher}";
+        }
+
+    }
+}
---
+++ ServiceStation/Classes/Autocar_LOCAL_1082.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceStation.Classes
+{
+    public abstract class Autocar
+    {
+        public AutoCompany ManufacturedCompany { get; private set; }
+        public string ModelCipher { get; private set; }
+
+        public EngineTypes EngineType { get; private set; }
+
+        public Autocar(AutoCompany manufacturedCompany, string modelCipher, EngineTypes engineTypes)
+        {
+            ManufacturedCompany = manufacturedCompany;
+            ModelCipher = modelCipher;
+            EngineType = engineTypes;
+        }
+
+        public virtual string GetFullName()
+        {
+            return $"{ManufacturedCompany}, {ModelCipher}";
+        }
+
+    }
+}
